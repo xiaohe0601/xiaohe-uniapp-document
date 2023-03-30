be3b41a268161474f3bc60b5ce4a9d6a65d98db5
MyHdg0601 <59353463+MyHdg0601@users.noreply.github.com>
2023-03-30 09:20:24 +0000

    Deploying to gh-pages from @ MyHdg0601/uniapp-vue2-hbx-starter-document@27f5767ccbe478615b85975afba27d0a688cb1a5 🚀

---
+++ assets/license.html-044c2939.js
@@ -0,0 +1,22 @@
+import{_ as e,W as i,X as n,a1 as s}from"./framework-b90dbfae.js";const t={},d=s(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>MIT License
+
+Copyright (c) 2022 MyHdg0601
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the &quot;Software&quot;), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),l=[d];function o(a,r){return i(),n("div",null,l)}const v=e(t,[["render",o],["__file","license.html.vue"]]);export{v as default};
